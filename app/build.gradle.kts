edbb893cc1d2a6daf7ac4046412e0311ff418cb2
matiny80 <matiny872@gmail.com>
2026-09-16 23:49:51 -0700

    add

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.devtools.ksp") version "2.0.21-1.0.27"
 }
 
 android {
@@ -56,4 +57,37 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation(libs.material3)
+
+    implementation(libs.androidx.navigation.compose)
+
+    implementation(libs.lifecycle.runtime.compose)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+
+    implementation(libs.androidx.room3.runtime)
+    implementation(libs.androidx.room.ktx)
+    ksp(libs.androidx.room3.compiler)
+
+    implementation("com.google.dagger:hilt-android:2.60.1")
+    ksp("com.google.dagger:hilt-compiler:2.60.1")
+
+    implementation("androidx.hilt:hilt-navigation-compose:1.4.0")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.11.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.11.0")
+
+    implementation("com.squareup.retrofit2:retrofit:3.0.0")
+    implementation("com.squareup.retrofit2:converter-gson:3.0.0")
+
+    implementation("com.squareup.okhttp3:okhttp:5.5.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:5.5.0")
+
+    implementation("androidx.datastore:datastore-preferences:1.2.1")
+
+    implementation("io.coil-kt.coil3:coil-compose:3.6.2")
+
+    implementation("androidx.paging:paging-runtime:3.5.1")
+    implementation("androidx.paging:paging-compose:3.5.1")
+
 }
